701ff5129e0a4c0be1b337d3af614532cd383fda
Juan Longo <juan.longo@mulesoft.com>
2024-05-15 00:06:33 -0300

    Enabling TP 7

---
+++ loops.py
@@ -0,0 +1,18 @@
+def index_of_by_index(word, list, index):
+    return -1
+
+
+def index_of_empty(list):
+    return -1
+
+
+def index_of(word, list):
+    return -1
+
+
+def put(word, list):
+    return -1
+
+
+def remove(word, list):
+    return -1
---
+++ loops_and_print.py
@@ -0,0 +1,6 @@
+def enumerate_list(list):
+    return list
+
+
+def enumerate_backwards(list):
+    return list
